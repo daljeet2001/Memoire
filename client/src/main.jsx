0f68adb339de45be3fc7e854977287e93b1f55ea
DALJEET SINGH <daljeetsingh@DALJEETs-MacBook-Air.local>
2025-08-27 23:23:10 +0530

    debugging

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import Dashboard from './pages/Dashboard'
 import Timeline from './pages/Timeline'
-import NewEntry from './pages/NewEntry'
+// import NewEntry from './pages/NewEntry'
 
 import LandingPage from './pages/LandingPage'
 import LoginPage from './pages/LoginPage'
@@ -18,7 +18,7 @@ const router = createBrowserRouter([
   { path: '/login', element: <LoginPage/> },
   { path: '/dashboard', element: <Dashboard/> },
   { path: '/timeline/:spaceId', element: <Timeline/> },
-  { path: '/new/:spaceId', element: <NewEntry/> },
+  // { path: '/new/:spaceId', element: <NewEntry/> },
   
 ])
 
